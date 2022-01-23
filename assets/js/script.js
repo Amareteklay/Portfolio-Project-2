90b793e6d1ec5e0317e75a5905ce3596fb825723
Amare Teklay Hailu <amareteklay@gmail.com>
2022-01-23 23:00:27 +0000

    add random computer choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,23 @@
 const paper = document.getElementById('paper');
 const computerScore = document.getElementById('compScore')
 const userScore = document.getElementById('userScore')
+
 const strategy = ['paper', 'rock', 'scissors', 'lizard', 'spock'];
 const winList = ['paperrock', 'rockscissors', 'scissorspaper', 'lizardpaper', 'spockrock', 'rocklizard', 'lizardspock', 'scissorslizard', 'paperspock', 'spockscissors'];
+
 let cscore = null;
 let uscore = null;
+
 function computerPicks() {
     let compChoice = Math.floor(Math.random()*5);
-    console.log(strategy[compChoice]);
-    updateScore();
-}
-
-function updateScore(){
+    console.log(compChoice)
+    if (strategy[compChoice] === 'paper') {
+        uscore += 1;
+        userScore.innerText = uscore;
+    } else {
         cscore += 1;
         computerScore.innerText = cscore;
+    }
 };
 
 paper.addEventListener('click', computerPicks);
-
