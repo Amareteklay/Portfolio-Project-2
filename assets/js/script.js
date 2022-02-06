561ea7b0757df40d94b27a94f7ae5d7d1d69474a
Amare Teklay Hailu <amareteklay@gmail.com>
2022-02-06 15:52:08 +0000

    add js function and feedback

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,45 @@
-const paper = document.getElementById('paper');
-const rock = document.getElementById('rock');
-const scissors = document.getElementById('scissors');
-const spock = document.getElementById('spock');
-const lizard = document.getElementById('lizard');
-
 const computerScore = document.getElementById('compScore')
 const userScore = document.getElementById('userScore')
 
-const yourChoice = document.getElementById('your-choice')
-const comChoice = document.getElementById('comp-choice')
-const clickedOnMe = document.getElementById('clicked-on')
+const yourChoice = document.getElementById('user-choice')
+const comChoice = document.getElementById('computer-choice')
+const winner = document.getElementById('winner')
 
-const strategy = ['paper', 'rock', 'scissors', 'lizard', 'spock']
+const strategy = document.getElementsByClassName('choice')
 const winList = ['paperrock', 'rockscissors', 'scissorspaper', 'lizardpaper', 'spockrock', 'rocklizard', 'lizardspock', 'scissorslizard', 'paperspock', 'spockscissors'];
 
 let cscore = null;
 let uscore = null;
 
 function computerPicks() {
-    let compChoice = Math.floor(Math.random()*5);
-    comChoice.innerHTML = strategy[compChoice];
-    let x = document.getElementById(strategy[compChoice])
-    x.style.backgroundColor = 'red';
-    youPick();
+    let randIndex = Math.floor(Math.random()*5);
+    return strategy[randIndex].id
 }
 
-function youPick(){
-    let choiceSet = document.getElementsByClassName('choice');
-    for (let i=0; i<choiceSet.length; i++) {
-        choiceSet[i].addEventListener('click', yeay);
+function play(){
+    for (let i=0; i<strategy.length; i++) {
+        strategy[i].addEventListener('click', determineWinner);
     }
 }
 
-function yeay(){
-    computerPicks();
-    console.log('You picked ' + this.id);
+function determineWinner(){
+    let computer = computerPicks();
+    let user = this.id;
+    let userComputer = user + computer;
+    if (winList.includes(userComputer)){
+        uscore +=1;
+        userScore.innerHTML = uscore;
+        yourChoice.innerHTML = user;
+        comChoice.innerHTML = computer;
+        winner.innerHTML = 'You Won!';
+    } else if (user === computer){
+        winner.innerHTML = 'It is draw';
+    }
+    else {
+        cscore +=1;
+        computerScore.innerHTML = cscore;
+        winner.innerHTML = 'You lost';
+    }
 }
 
-youPick();
+play();
