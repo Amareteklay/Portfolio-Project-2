f4c042db27eb82b2229720d380e62c59890c5345
Amare Teklay Hailu <amareteklay@gmail.com>
2022-02-06 14:19:59 +0000

    add event listener by looping

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,17 @@
 const paper = document.getElementById('paper');
+const rock = document.getElementById('rock');
+const scissors = document.getElementById('scissors');
+const spock = document.getElementById('spock');
+const lizard = document.getElementById('lizard');
+
 const computerScore = document.getElementById('compScore')
 const userScore = document.getElementById('userScore')
 
-const strategy = ['paper', 'rock', 'scissors', 'lizard', 'spock'];
+const yourChoice = document.getElementById('your-choice')
+const comChoice = document.getElementById('comp-choice')
+const clickedOnMe = document.getElementById('clicked-on')
+
+const strategy = ['paper', 'rock', 'scissors', 'lizard', 'spock']
 const winList = ['paperrock', 'rockscissors', 'scissorspaper', 'lizardpaper', 'spockrock', 'rocklizard', 'lizardspock', 'scissorslizard', 'paperspock', 'spockscissors'];
 
 let cscore = null;
@@ -10,14 +19,22 @@ let uscore = null;
 
 function computerPicks() {
     let compChoice = Math.floor(Math.random()*5);
-    console.log(compChoice)
-    if (strategy[compChoice] === 'paper') {
-        uscore += 1;
-        userScore.innerText = uscore;
-    } else {
-        cscore += 1;
-        computerScore.innerText = cscore;
-    }
-};
+    comChoice.innerHTML = strategy[compChoice];
+    let x = document.getElementById(strategy[compChoice])
+    x.style.backgroundColor = 'red';
+    youPick();
+}
 
-paper.addEventListener('click', computerPicks);
+function youPick(){
+    let choiceSet = document.getElementsByClassName('choice');
+    for (let i=0; i<choiceSet.length; i++) {
+        choiceSet[i].addEventListener('click', yeay);
+    }
+}
+
+function yeay(){
+    computerPicks();
+    console.log('You picked ' + this.id);
+}
+
+youPick();
